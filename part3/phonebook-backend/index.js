82381a2f40f833d57c5f59b681dd3af7729f0e61
John Loper <john.and.loper@gmail.com>
2020-07-13 16:11:26 -0700

    3.8 added morgan logger

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const { response } = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('post-body', (req, res) => { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
 let persons = [
     {
