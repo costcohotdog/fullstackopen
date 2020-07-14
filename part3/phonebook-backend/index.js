501cbf58b675ae4dfe8d3061705d8732b9f393c6
John Loper <john.and.loper@gmail.com>
2020-07-14 10:27:31 -0700

    3.11, deployed to heroku

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const { response } = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('post-body', (req, res) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
